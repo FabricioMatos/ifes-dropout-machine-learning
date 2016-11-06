e2a21de17f100b427f4d88be9fea3b6d5e23bf56
Fabricio Matos <fabriciovargasmatos@gmail.com>
2016-11-06 02:09:30 -0200

    initial files

---
+++ lib/eda1.py
@@ -0,0 +1,16 @@
+# License: BSD 3 clause <https://opensource.org/licenses/BSD-3-Clause>
+# Copyright (c) 2016, Fabricio Vargas Matos <fabriciovargasmatos@gmail.com>
+# All rights reserved.
+
+''''
+Run the first exploratory data analysis in order to underdand better the data available and plan the next steps.
+'''
+
+
+def run():
+    print '<<< Running Exploratory Data Analysis #1 ==='
+
+    #do something
+    
+    print '=== Running Exploratory Data Analysis #1 >>>'
+
---
+++ main.py
@@ -0,0 +1,20 @@
+# License: BSD 3 clause <https://opensource.org/licenses/BSD-3-Clause>
+# Copyright (c) 2016, Fabricio Vargas Matos <fabriciovargasmatos@gmail.com>
+# All rights reserved.
+
+'''
+Project: IFES dropout prediction
+
+Use Machine Learning techniques to model a student's dropout prediction system for Brazilian Federal Institutes of Education, Science and Technology analyzing data available in the Q-Academico information system.
+'''
+
+
+# Load libraries
+import lib.eda1 as eda1
+
+print '<<< main ==='
+
+#run the exploratory data analyses #1
+eda1.run()
+
+print '=== main >>>'
